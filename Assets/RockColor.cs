f598cc0dd48173272f2d0842cd05b64af80d4d4c
William Osborne <wosborne@live.co.uk>
2016-09-12 17:50:17 +0100

    Space level

---
+++ Assets/RockColor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RockColor : MonoBehaviour {
+
+	public Material[] materials;
+
+	// Use this for initialization
+	void Start () {
+
+		int colorPic = Random.Range(0, materials.Length);
+
+		Renderer rend = GetComponent<Renderer> ();
+		if (rend != null){
+			rend.material = materials[colorPic];
+			Debug.Log (materials);
+			Debug.Log (colorPic);
+		}
+
+
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
